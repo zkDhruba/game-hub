fbdbfe068ed05613fb70cb5db867e3c01ea0cd9d
Dhruba Karim <dhruba@ussbdpos.com>
2024-04-23 16:48:39 +0600

    implemented canceled error for fetching game api

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
+import { CanceledError } from "axios";
 
 interface Game {
     id: number;
@@ -16,10 +17,17 @@ const useGames = () => {
     const [error, setError] = useState("");
 
     useEffect(() => {
+        const controller = new AbortController();
+
         apiClient
-        .get<FetchGamesResponse>("/games")
-        .then((res) => setGames(res.data.results))
-        .catch((err) => setError(err.message));
+            .get<FetchGamesResponse>("/games", {signal: controller.signal })
+            .then((res) => setGames(res.data.results))
+            .catch((err) => {
+                if (err instanceof CanceledError) return;
+                setError(err.message)
+            });
+        return () => controller.abort();
+     
     }, []);
 
     return { games, error }
